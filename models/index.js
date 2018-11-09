f8a2f9cdcc1db7d09aaffc75d5a59814df6a1d81
Kara Garvey <karagarvey123@gmail.com>
2018-11-09 12:47:38 -0500

    views folder

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const Page = db.define('page', {
+  title: Sequelize.STRING,
+  slug: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlphanumeric: true
+    }
+  },
+  content: Sequelize.STRING,
+  status: Sequelize.STRING
+})
+
+const User = db.define('user', {
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }
+})
+
+
+module.exports = {
+  db
+}
