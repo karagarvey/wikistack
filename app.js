68bcee2ec233f66e328e0964acda39dac3195073
Kara Garvey <karagarvey123@gmail.com>
2018-11-09 12:46:49 -0500

    models built

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const layout = require("./views/layout.js");
+const { db } = require('./models');
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-app.use("/main", require("./views/main.js"));
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.get("/", (req, res) => {
   res.send(layout(""));
 });
+
 
 const port = 8080;
 app.listen(port, () => {
