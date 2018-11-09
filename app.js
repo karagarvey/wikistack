651c0a67c89cf3fb2f40223fbae6ac9366f60da4
Michelle <mcurena24@gmail.com>
2018-11-09 14:30:41 -0500

    requiring table info into app.js

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const layout = require("./views/layout.js");
-const { db } = require('./models');
+const data = require("./models");
+console.log(data.db);
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-})
+data.db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.get("/", (req, res) => {
   res.send(layout(""));
 });
-
 
 const port = 8080;
 app.listen(port, () => {
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack");
 
-const Page = db.define('page', {
+const Page = db.define("page", {
   title: Sequelize.STRING,
   slug: {
     type: Sequelize.STRING,
@@ -10,10 +10,10 @@ const Page = db.define('page', {
     }
   },
   content: Sequelize.STRING,
-  status: Sequelize.STRING
-})
+  type: Sequelize.ENUM("open", "closed")
+});
 
-const User = db.define('user', {
+const User = db.define("user", {
   name: Sequelize.STRING,
   email: {
     type: Sequelize.STRING,
@@ -21,9 +21,6 @@ const User = db.define('user', {
       isEmail: true
     }
   }
-})
+});
 
-
-module.exports = {
-  db
-}
+module.exports = { db, Page, User };
