2329376c70fea4a3800f151cca3d800f0bdf6b1e
Michelle <mcurena24@gmail.com>
2018-11-09 12:16:49 -0500

    Completed main route

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-app.use(express.urlencoded({extended: false}))
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const layout = require("./views/layout.js");
+app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan('dev'))
-app.use(express.static(__dirname + '/public'))
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-  res.send('hello world!')
-})
+app.use("/main", require("./views/main.js"));
 
-const port = 8080
+app.get("/", (req, res) => {
+  res.send(layout(""));
+});
+
+const port = 8080;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
